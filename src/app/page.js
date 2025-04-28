fd4f522a3fee09bc46ed1daff05e946deb5f8a22
Caesar Arssetya <38849354+caesarars@users.noreply.github.com>
2025-04-28 15:21:50 +0700

    add sitemap

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,36 @@ import CryptoSentiment from "./component/sentimentCoins/CryptoSentiment";
 import TrendingCoins from "./component/trendingCoins/TrendingCoins";
 import "./page.css"
 
+export const metadata = {
+  title: "CryptoBriefs - Latest Crypto News, Insights, and Trends",
+  description: "Stay updated with CryptoBriefs – your go-to source for cryptocurrency news, education, and trending market insights. Discover Bitcoin, Ethereum, and the latest Web3 projects.",
+  keywords: ["Crypto News", "Cryptocurrency Updates", "Bitcoin", "Ethereum", "Web3", "Crypto Trends", "Blockchain News"],
+  openGraph: {
+    title: "CryptoBriefs - Latest Crypto News and Insights",
+    description: "Explore the latest crypto news, education, and market trends on CryptoBriefs.",
+    url: "https://cryptobriefs.net",
+    siteName: "CryptoBriefs",
+    images: [
+      {
+        url: "https://cryptobriefs.net/og-image.jpg", // Ganti ke image OG kamu
+        width: 1200,
+        height: 630,
+        alt: "CryptoBriefs OG Image",
+      },
+    ],
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
+  alternates: {
+    canonical: "https://cryptobriefs.net",
+  },
+};
+
+
 export default function Home({  }) {
   return (
     <>
